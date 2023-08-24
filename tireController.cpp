2c3b44ef69b9051077cfbf742fc9aca828f36b10
Arma Burton <arma@armaburton.com>
2023-08-24 09:19:21 -0700

    init

--- tireController.cpp
+++ tireController.cpp
@@ -3,6 +3,22 @@
 
 bool loadData(TireData list[], int &count)
 {
+    ifstream infile;
+    if (!infile)
+    {
+        cout << "File did not open. Program exiting.\n";
+        return false;
+    }
 
-        return true;
+    while (infile.getline(list[count].dimensions, MAXLEN, ';'))
+    {
+        infile.getline(list[count].brand, MAXLEN, ';');
+        infile.getline(list[count].style, MAXLEN, ';');
+        infile >> list[count].price;
+        infile.ignore(100, '\n');
+        count++;
+    }
+    infile.close();
+
+    return true;
 }
